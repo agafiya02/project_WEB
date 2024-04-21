bfb5f115e9ecec230fe90b7856d06e42ec786fcd
agafiya02 <151033863+agafiya02@users.noreply.github.com>
2024-04-21 16:13:15 +0500

    Delete data/form.py

--- data/form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SubmitField
-
-
-class Form(FlaskForm):
-    menu = SubmitField('Войти')
-    about_us = SubmitField('Войти')
-    basket = SubmitField('Войти')
