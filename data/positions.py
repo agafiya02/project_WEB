5da50c9f0c28a193d6c563f6d6e76964ce8526ab
agafiya02 <151033863+agafiya02@users.noreply.github.com>
2024-04-21 16:13:30 +0500

    Add files via upload

---
+++ data/positions.py
@@ -0,0 +1,14 @@
+import sqlalchemy
+from flask_login import UserMixin
+from .session import SqlAlchemyBase
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class Position(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'menu'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    price = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
